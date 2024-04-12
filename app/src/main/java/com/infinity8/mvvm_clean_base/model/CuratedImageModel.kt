91367707609941be78a2e6cabf0b34cc1fc8adde
ved <ved.prakash@neosoftmail.com>
2024-04-12 18:50:08 +0530

    add parcelable

--- app/src/main/java/com/infinity8/mvvm_clean_base/model/CuratedImageModel.kt
+++ app/src/main/java/com/infinity8/mvvm_clean_base/model/CuratedImageModel.kt
@@ -1,5 +1,11 @@
 package com.infinity8.mvvm_clean_base.model
 
+import android.os.Parcelable
+import androidx.annotation.Keep
+import kotlinx.parcelize.Parcelize
+
+@Keep
+@Parcelize
 data class CuratedImageModel(
     val next_page: String?,
     val page: Int,
@@ -7,8 +13,10 @@ data class CuratedImageModel(
     val photos: List<Photo>,
     val prev_page: String?,
     val total_results: Int
-)
+) : Parcelable
 
+@Keep
+@Parcelize
 data class Photo(
     val alt: String,
     val avg_color: String?,
@@ -21,7 +29,7 @@ data class Photo(
     val src: Src,
     val url: String,
     val width: Int
-) {
+) : Parcelable {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
@@ -34,6 +42,8 @@ data class Photo(
     }
 }
 
+@Keep
+@Parcelize
 data class Src(
     val landscape: String?,
     val large: String?,
@@ -43,4 +53,4 @@ data class Src(
     val portrait: String?,
     val small: String?,
     val tiny: String?
-)
+) : Parcelable
